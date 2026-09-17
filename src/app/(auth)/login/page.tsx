f94f46ef47643ede3160fc99435052dfd0e5183f
ShadyyJoestar <revanshauqie@gmail.com>
2026-09-17 21:06:42 +0700

    try bug fix

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
 import { LoginForm } from "@/components/auth/login-form";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div className="w-full max-w-md">
+      <LoginForm />
+    </div>
+  );
 }
